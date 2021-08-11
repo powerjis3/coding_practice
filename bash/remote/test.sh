632d93fbd6b138a47e4fafc77b1bf9e70ec20a2f
hjshin <powerjis3@gmail.com>
2021-08-11 08:32:59 +0000

    value edit

--- bash/remote/test.sh
+++ bash/remote/test.sh
@@ -93,36 +93,31 @@ while read IP
 do
 
 ## OS check ##
-OS_ISSUE=$(sshpass -p $PASSWORD ssh -o ConnectTimeout=2 $USER_ID@$IP  -no StrictHostKeyChecking=no "cat /etc/issue | awk '{print \$1}' | sed -n 1p | tr '[A-Z]' '[a-z]'" 2>&1)
-OS_VERSION=$(sshpass -p $PASSWORD ssh -o ConnectTimeout=2 $USER_ID@$IP  -no StrictHostKeyChecking=no "cat /etc/redhat-release | sed 's/[a-z,A-Z]//g' | awk '{print \$1}'" 2>&1)
+OS_ISSUE=$(sshpass -p $PASSWORD ssh -o ConnectTimeout=2 $USER_ID@$IP  -no StrictHostKeyChecking=no "cat /etc/issue | awk '{print \$1}' | sed -n 1p | tr '[A-Z]' '[a-z]'" 2>/dev/null)
+OS_VERSION=$(sshpass -p $PASSWORD ssh -o ConnectTimeout=2 $USER_ID@$IP  -no StrictHostKeyChecking=no "cat /etc/redhat-release | sed 's/[a-z,A-Z]//g' | awk '{print \$1}'" 2>/dev/null)
 
 ## user check
 SSH_ALLOW_CHECK=$(sshpass -p $PASSWORD ssh -o ConnectTimeout=2 $USER_ID@$IP  -no StrictHostKeyChecking=no "echo $PASSWORD | sudo -S cat /root/tmp/sshd_config | grep AllowUsers | grep $4@$5 | wc -l" 2>/dev/null)
-CENTOS_GROUP_CHECK=$(sshpass -p $PASSWORD ssh $USER_ID@$IP  -no StrictHostKeyChecking=no  "echo $PASSWORD | sudo -S cat /root/tmp/group | grep wheel | grep $4 | wc -l" 2>/dev/null)
-UBUNTU_GROUP_CHECK=$(sshpass -p $PASSWORD ssh $USER_ID@$IP  -no StrictHostKeyChecking=no  "echo $PASSWORD | sudo -S cat /root/tmp/group | grep sudo | grep $4 | wc -l" 2>/dev/null)
+CENTOS_GROUP_CHECK="sshpass -p $PASSWORD ssh $USER_ID@$IP  -no StrictHostKeyChecking=no  echo $PASSWORD | sudo -S cat /root/tmp/group | grep wheel | grep $4 | wc -l"
+UBUNTU_GROUP_CHECK="sshpass -p $PASSWORD ssh $USER_ID@$IP  -no StrictHostKeyChecking=no  echo $PASSWORD | sudo -S cat /root/tmp/group | grep sudo | grep $4 | wc -l"
 
 ## user add ##
-SSH_ALLOW_COMMEND="echo $PASSWORD | sudo -S sed -ie '/^AllowUsers/ s/$/ $4@$5/' /root/tmp/sshd_config"
-SSH_ALLOW_ADD="sshpass -p $PASSWORD ssh -o ConnectTimeout=2 $USER_ID@$IP  -no StrictHostKeyChecking=no $SSH_ALLOW_COMMEND"
-SUDO_GROUP_ADD="echo $PASSWORD | sudo -S sed -ie '/^sudo:/ s/$/,$4/' /root/tmp/group"
-UBUNTU_GROUP_ADD="sshpass -p $PASSWORD ssh $USER_ID@$IP -no StrictHostKeyChecking=no $SUDO_GROUP_ADD"
-WHEEL_GROUP_ADD="echo $PASSWORD | sudo -S sed -ie '/^wheel:/ s/$/,$4/' /root/tmp/group"
-CENTOS_GROUP_ADD="sshpass -p $PASSWORD ssh $USER_ID@$IP -no StrictHostKeyChecking=no $WHEEL_GROUP_ADD"
-SYSTEMD_COMMEND="echo $PASSWORD | sudo -S systemctl restart sshd"
-SSH_RESTART_SYSTEMD="sshpass -p $PASSWORD ssh $USER_ID@$IP  -no StrictHostKeyChecking=no  $SYSTEMD_COMMEND"
-INITD_COMMEND="echo $PASSWORD | sudo -S /etc/init.d/sshd restart"
-SSH_RESTART_INITD="sshpass -p $PASSWORD ssh $USER_ID@$IP  -no StrictHostKeyChecking=no  $INITD_COMMEND"
+SSH_ALLOW_ADD="sshpass -p $PASSWORD ssh $USER_ID@$IP  -no StrictHostKeyChecking=no echo $PASSWORD | sudo -S sed -ie '/^AllowUsers/ s/$/ $4@$5/' /root/tmp/sshd_config"
+UBUNTU_GROUP_ADD="sshpass -p $PASSWORD ssh $USER_ID@$IP -no StrictHostKeyChecking=no echo $PASSWORD | sudo -S sed -ie '/^sudo:/ s/$/,$4/' /root/tmp/group"
+CENTOS_GROUP_ADD="sshpass -p $PASSWORD ssh $USER_ID@$IP -no StrictHostKeyChecking=no echo $PASSWORD | sudo -S sed -ie '/^wheel:/ s/$/,$4/' /root/tmp/group"
+SSH_RESTART_SYSTEMD="sshpass -p $PASSWORD ssh $USER_ID@$IP  -no StrictHostKeyChecking=no echo $PASSWORD | sudo -S systemctl restart sshd"
+SSH_RESTART_INITD="sshpass -p $PASSWORD ssh $USER_ID@$IP  -no StrictHostKeyChecking=no echo $PASSWORD | sudo -S /etc/init.d/sshd restart"
 
 
 echo -n "$IP" | tee -a result.txt
 
-if [ -z $OS_ISSUE ];then
-    echo '' >> result.txt
+if [[ -z $OS_ISSUE ]];then
+    echo ' - connect fail' | tee -a result.txt
     continue
-elif [ $OS_ISSUE = ubuntu ];then
+elif [[ $OS_ISSUE = ubuntu ]];then
     echo -n ' - connect ubuntu server' | tee -a result.txt
     if [ $3 = group ];then
-        if [ $UBUNTU_GROUP_CHECK != 0 ];then
+        if [[ $($UBUNTU_GROUP_CHECK) != 0 ]];then
             echo ' - The group_user value already exists - fail' | tee -a result.txt
             continue
         else
@@ -140,8 +135,8 @@ elif [ $OS_ISSUE = ubuntu ];then
             $SSH_RESTART_SYSTEMD 2>/dev/null
         fi
     else
-        if [ $SSH_ALLOW_CHECK != 0 ]||[ $UBUNTU_GROUP_CHECK != 0 ];then
-            if [ $SSH_ALLOW_CHECK != 0 ]&&[ $UBUNTU_GROUP_CHECK != 0 ];then
+        if [ $SSH_ALLOW_CHECK != 0 ]||[[ $($UBUNTU_GROUP_CHECK) != 0 ]];then
+            if [ $SSH_ALLOW_CHECK != 0 ]&&[[ $($UBUNTU_GROUP_CHECK) != 0 ]];then
                 echo ' - The ssh/group_user value already exists - fail' | tee -a result.txt
                 continue
             elif [ $SSH_ALLOW_CHECK != 0 ];then
@@ -162,7 +157,7 @@ elif [ $OS_ISSUE = ubuntu ];then
 else
     echo -n " - connect centos server" | tee -a result.txt
     if [ $3 = group ];then
-        if [ $CENTOS_GROUP_CHECK != 0 ];then
+        if [[ $($CENTOS_GROUP_CHECK) != 0 ]];then
             echo ' - The group_user value already exists - fail' | tee -a result.txt
             continue
         else
@@ -185,8 +180,8 @@ else
             fi
         fi
     else
-        if [ $SSH_ALLOW_CHECK != 0 ]||[ $CENTOS_GROUP_CHECK != 0 ];then
-            if [ $SSH_ALLOW_CHECK != 0 ]&&[ $CENTOS_GROUP_CHECK != 0 ];then
+        if [ $SSH_ALLOW_CHECK != 0 ]||[[ $($CENTOS_GROUP_CHECK) != 0 ]];then
+            if [ $SSH_ALLOW_CHECK != 0 ]&&[[ $($CENTOS_GROUP_CHECK) != 0 ]];then
                 echo ' - The ssh/group_user value alrready exists - fail' | tee -a result.txt
                 continue
             elif [ $SSH_ALLOW_CHECK != 0 ];then
@@ -227,6 +222,6 @@ cat /dev/null > result.txt
 LOGIN
 ADD_PROCESS $1 $2 $3 $4 $5 
 
-### remove spaces ###
+### remove space ###
 cat result.txt | grep -v -E ^$ >> result.tmp
 mv result.tmp result.txt
